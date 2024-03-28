34b21d830eaf15c002bb7450a6ebb33c379704f0
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-28 10:56:19 +0100

    task

---
+++ task.js
@@ -0,0 +1,22 @@
+let x = 4
+let y = 5
+
+console.log((x + y) * 2)
+
+let value = 19
+console.log(value +(value % 10))
+
+let firstValue = 4
+
+for (let n = 0; n < 4; n++) {
+    firstValue *= 4
+}
+console.log(firstValue)
+
+
+let a = 30
+let x2 = a + 10
+let y2 = a - 10
+let z = a % 10
+console.log(z)
+console.log(a / (x2 * y2 * z))
