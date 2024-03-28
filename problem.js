b7e9ce9a82bda3e9b5502ca788541de4389a3d23
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-28 15:01:59 +0100

    task

--- problem.js
+++ problem.js
@@ -3,14 +3,9 @@ let right_operand = 11
 operator = "*"
 ans = 0
 
-if (operator === "+") {
-    ans = left_operand + right_operand
-} else if (operator === "-") {
-    ans = left_operand - right_operand
-} else if (operator === "*") {
-    ans = left_operand * right_operand
-} else if (operator === "/") {
-    ans = left_operand / right_operand
-}
+if (operator === "+") ans = left_operand + right_operand
+else if (operator === "-") ans = left_operand - right_operand
+else if (operator === "*") ans = left_operand * right_operand
+else if (operator === "/") ans = left_operand / right_operand
 
 console.log(ans)
