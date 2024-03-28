28f8fb196dedcd0457364589d9a8045ace1d0429
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-28 14:29:17 +0100

    task

---
+++ librarian.js
@@ -0,0 +1,9 @@
+let librarian = {
+    id: 11234,
+    available: true,
+    count: 100,
+    name: "Victor Msonte",
+    author: "Victor Msonte",
+}
+
+console.log(librarian.id + "\n" + librarian.name);
