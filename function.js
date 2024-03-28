e7c4d50e889768eadc82c1028a3d89f94a9ef065
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-28 15:35:07 +0100

    task

--- function.js
+++ function.js
@@ -1,8 +1,20 @@
-function solve(x) {
+
+// console.log(solve("3")("2", "4"))
+// function solve(x) {
+//     function secondNumber(a, e){
+//         return a * x * e;
+//     }
+//     return secondNumber;
+// }
+//
+// console.log(solve(5)(3, 2));
+
+
+const calc = function (x) {
     function secondNumber(a, e){
         return a * x * e;
     }
     return secondNumber;
 }
 
-console.log(solve(5)(3, 2));
+console.log(calc(3)(2, 3))
