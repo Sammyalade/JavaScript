25792f720baa35499105b4418b02599be7ec954d
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-17 16:26:08 +0100

    task

---
+++ task2.js
@@ -0,0 +1,76 @@
+var lagos = {
+    name: "Lagos",
+    population: 100,
+    temp: 10,
+    currency: "Naira"
+}
+
+var abuja = {
+    name: "Abuja",
+    population: 80,
+    temp: 5,
+    currency: "Naira"
+}
+
+var kaduna = {
+    name: "Kaduna",
+    population: 80,
+    temp: 8,
+    currency: "Naira"
+}
+
+var ekiti = {
+    name: "Ekiti",
+    population: 80,
+    temp: 8,
+    currency: "Naira"
+}
+
+var benue = {
+    name: "Benue",
+    population: 70,
+    temp: 8,
+    currency: "Naira"
+}
+
+var oyo = {
+    name: "Oyo",
+    population: 800,
+    temp: 8,
+    currency: "Naira"
+}
+
+var ibadan = {
+    name: "IBadan",
+    population: 840,
+    temp: 8,
+    currency: "Naira"
+}
+
+var delta = {
+    name: "Delta",
+    population: 807,
+    temp: 8,
+    currency: "Naira"
+}
+
+var uyo = {
+    name: "Uyo",
+    population: 500,
+    temp: 8,
+    currency: "Naira"
+}
+
+let raw_array = [[null, lagos, uyo, ibadan], [kaduna, oyo, abuja, null], [delta, null, ekiti, benue]]
+
+let total_population = 0;
+for(const value of raw_array){
+    for(const index in value){
+        if(value[index] != null){
+            total_population += value[index].population;
+        }
+    }
+}
+
+
+console.log(total_population);
